b42178ef6dc6e84756ba5e318e1389e4e4bf9b82
LMC123456LMC <20227032@stu.neu.edu.cn>
2025-02-16 22:44:44 +0800

    Update model_result_stats.py

--- statistics_scripts/model_result_stats.py
+++ statistics_scripts/model_result_stats.py
@@ -1,13 +1,11 @@
-# 统计
-
 import json
 import re
 from tqdm import tqdm
 import sys
 
 model = "gpt_4o_mini"
-f_res='/data/yuchen_llm_eval/data/新的验证结果/full_res_gpt_4o_mini_0213_01.json'
-log_file_path = "/data/yuchen_llm_eval/data/新的验证结果/full_res_gpt_4o_mini_0213_01_log.txt"
+f_res=''  # filename of data
+log_file_path = ""  # filename of output log
 sys.stdout = open(log_file_path, 'w', encoding='utf-8')
 
 model_div_string_dict={
@@ -38,40 +36,40 @@ if content.endswith(','):
     print('end with comma')
     content=content[:-1]+']'
 data_res = json.loads(content)
-print(model, '结果数目:',len(data_res))
+print(model, 'Result count:',len(data_res))
 prompts = []
 for it in data_res:
     prompts.append(it['prompt'])
-print(model, '独特prompt数目:',len(set(prompts)))
+print(model, 'unique prompt count:',len(set(prompts)))
 print('-----------------')
-print('不规则输出：')
+print('Irregular output：')
 
-# 获取正确答案
+# get right answer
 def get_right_answer(res:dict, raw_answer: str):
 
-    # :res: 模型输出结果
-    # :param raw_answer: 完整原回答
-    # :return: 此位置上的正确答案候选
+    # :res: Model output
+    # :param raw_answer: Complete original answer
+    # :return: The correct answer candidate for this position
 
-    #div_string="<user_end><im_assistant>" # 闭源模型
-    #div_string="形式如'[abcd1234]'：\n" # 豆包——oldprompt
-    #div_string = "<|im_end|>\n<|im_start|>assistant\n"  # 千问
+    #div_string="<user_end><im_assistant>" # Closed-source model
+    #div_string="形式如'[abcd1234]'：\n" # doubao——oldprompt
+    #div_string = "<|im_end|>\n<|im_start|>assistant\n"  # qwen
     #div_string = "\n\nAssistant:"  # deepseek
     #div_string = "<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n"  # Llama
     #div_string="<|assistant|>\n" # glm
     div_string = model_div_string_dict[model]
     
-    prefix = res.get('prompt').partition(div_string)[-1][:-1] # 去掉"["
+    prefix = res.get('prompt').partition(div_string)[-1][:-1] # delete"["
     answer_after_prefix = raw_answer.partition(prefix)[-1]
     answer_candidates = []
     if prefix not in raw_answer:
-        print('错误匹配，原文无本句')
-        print("句子: ",prefix)
+        print('Incorrect match, no sentence in the original text')
+        print("sentence: ",prefix)
         return []
     while True:
-        #匹配文本anser_after_prefix中的第一个形如[abcd1234]的引证
+        # Matches the citation for the first shape like [abcd1234] in the text anser_after_prefix
         candidate = re.match(r"\[([a-zA-Z0-9]{8})\]", answer_after_prefix)
-        #如果有，就去掉这个引证，匹配下一个引证。
+        # If there is one, remove that quote and match it to the next.
         if candidate:
             answer_candidates.append(candidate.group())
             answer_after_prefix = answer_after_prefix.partition(candidate.group())[-1]
@@ -119,7 +117,7 @@ for res in data_res:
             irrg+=1
             print("response: ", response)
             print("     correct answer: ", correct_answer)
-            #continue # 删掉错误数据
+            #continue # Delete the wrong data
         dic['category']=category
         dic['output']=output
         dic['prompt']=prompt
@@ -135,12 +133,12 @@ for res in data_res:
         new_res.append(dic)
 
 print('-----------------')
-print('引证题目数目:', cnt) # 处理的条目数量
-print('不规则数据数目:', irrg) # 有问题的条目数量
-print(model, '模型引证正确数目:', corr)  # 输出模型正确的引证数目
-print(model, '模型引证正确率:', round(corr / cnt * 100, 2)) 
+print('citation count:', cnt) # Number of entries processed
+print('Irregular data number:', irrg) # Number of problematic entries
+print(model, 'Models cite correct numbers:', corr)  # Output the correct number of citations for the model
+print(model, 'Correct rate of model citation:', round(corr / cnt * 100, 2)) 
 
 new_outputs=[it['output'] for it in new_res]
-print('去除不规则数据之后的输出数量:',len(set(new_outputs)))
+print('The number of outputs after removing irregular data:',len(set(new_outputs)))
 
 sys.stdout.close()
